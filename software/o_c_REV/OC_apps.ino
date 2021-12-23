80d2dc4accb72c457448278793d0332c08cca1e8
Ben Rosenbach <ben@benrosenbach.com>
2021-12-23 14:50:28 -0500

    Remove apps and updating app count

--- software/o_c_REV/OC_apps.ino
+++ software/o_c_REV/OC_apps.ino
@@ -37,8 +37,8 @@
 OC::App available_apps[] = {
   DECLARE_APP('H','S', "Hemisphere", HEMISPHERE),
   // DECLARE_APP('M','I', "Captain MIDI", MIDI),
-  DECLARE_APP('D','2', "Darkest Timeline", TheDarkestTimeline),
-  DECLARE_APP('E','N', "Enigma", EnigmaTMWS),
+  // DECLARE_APP('D','2', "Darkest Timeline", TheDarkestTimeline),
+  // DECLARE_APP('E','N', "Enigma", EnigmaTMWS),
   // DECLARE_APP('N','N', "Neural Net", NeuralNetwork),
   DECLARE_APP('S','C', "Scale Editor", SCALEEDITOR),
   DECLARE_APP('W','A', "Waveform Editor", WaveformEditor),
