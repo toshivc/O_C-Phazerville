719f0f0379459e86838feff21235692375820adb
herrkami <aebsor@gmail.com>
2022-02-08 00:05:08 +0100

    Replace FM snare by bandpass noise snare

--- software/o_c_REV/HEM_BootsNCat.ino
+++ software/o_c_REV/HEM_BootsNCat.ino
@@ -20,8 +20,11 @@
 // SOFTWARE.
 
 // TODO:
-// - blend is a stupid parameter
-// - check the scaling of envelops and modulators... there emight be cleaner ways than HEMISPHERE_3V_CV etc.
+// - check the scaling of envelops and modulators... there might be cleaner ways than HEMISPHERE_3V_CV etc.
+// - clean up int64, int32, etc.
+// - remove blend and add input to CV mux: CV can control tone, level, or delay
+// - display
+// - improve bang icon
 
 #include "vector_osc/HSVectorOscillator.h"
 #include "vector_osc/WaveformManager.h"
@@ -43,20 +46,19 @@ public:
 
     void Start() {
         tone_kick = 32; // Kick drum freq
-        decay_kick = 32; // Kick drum decay
-        punch = BNC_MAX_PARAM/2; // Kick drum punch
+        decay_kick = 60; // Kick drum decay
+        punch = 50; // Kick drum punch
         decay_punch = 32; // Kick drum punch decay
 
-        tone_snare = 55; // Snare low limit
-        decay_snare = 16; // Snare decay
-        decay_snap = 32;
-        dirt = 32;
-        noise_tone_countdown = 1;
+        tone_snare = 6;
+        decay_snare = 50; // Snare decay
+        snap = 55;
+        decay_snap = 26;
 
         blend = 0;
 
         kick = WaveformManager::VectorOscillatorFromWaveform(HS::Sine);
-        SetBDFreq();
+        SetFreqKick();
         // Audio signal is -3V to +3V due to DAC asymmetry
         kick.SetScale((12 << 7) * 3);
 
@@ -85,18 +87,6 @@ public:
         env_snap.Offset(HEMISPHERE_3V_CV);
         env_snap.Cycle(0);
         SetEnvDecaySnap();
-
-        snare_mod0 = WaveformManager::VectorOscillatorFromWaveform(HS::Triangle);
-        snare_mod0.SetFrequency(FREQ_SNARE_MOD0);
-        snare_mod0.SetScale(HEMISPHERE_3V_CV);
-
-        snare_mod1 = WaveformManager::VectorOscillatorFromWaveform(HS::Sine);
-        snare_mod1.SetFrequency(FREQ_SNARE_MOD1);
-        snare_mod1.SetScale(HEMISPHERE_3V_CV);
-
-        snare = WaveformManager::VectorOscillatorFromWaveform(HS::Sine);
-        snare.SetFrequency(661*100); // 100 Hz - 1500 Hz
-        snare.SetScale(HEMISPHERE_3V_CV);
     }
 
     void Controller() {
@@ -105,8 +95,11 @@ public:
         int32_t sd_signal = 0;
 
         // Calculate kick drum signal
-        if (Changed(CH_KICK))
-            env_kick.SetScale(HEMISPHERE_MAX_CV - In(CH_KICK));
+        if (Changed(CH_KICK)) {
+            int scale = HEMISPHERE_MAX_CV - In(CH_KICK);
+            scale = scale < 0 ? 0 : scale;
+            env_kick.SetScale(scale);
+        }
         if (Clock(CH_KICK, 1)) {  // Use physical-only clocking
             env_kick.Start();
             env_punch.Start();
@@ -115,57 +108,52 @@ public:
         if (!env_kick.GetEOC()) {
             // base frequency
             int freq_kick = Proportion(tone_kick, BNC_MAX_PARAM, 3000) + 3000;
-
             // punchy FM drop
             if (!env_punch.GetEOC()) {
                 int df = Proportion(env_punch.Next(), HEMISPHERE_3V_CV, freq_kick);
                 df = Proportion(punch, BNC_MAX_PARAM/4, df);
-                freq_kick = freq_kick + df;
+                freq_kick += df;
             }
-
             kick.SetFrequency(freq_kick);
             levels[0] = env_kick.Next()/2; // Divide by 2 to account for offset
+            // int32_t _kick = kick.Next() + Proportion(8, 1024, noise);
+            // bd_signal = Proportion(levels[0], HEMISPHERE_MAX_CV, _kick);
             bd_signal = Proportion(levels[0], HEMISPHERE_MAX_CV, kick.Next());
         }
 
         // Calculate snare drum signal
-        // if (--noise_tone_countdown == 0) {
-        //     noise = random(0, (12 << 7) * 6) - ((12 << 7) * 3);
-        //     noise_tone_countdown = BNC_MAX_PARAM - tone_snare + 1;
-        // }
-        if (Changed(CH_SNARE))
-            env_snare.SetScale(HEMISPHERE_3V_CV - In(CH_SNARE));
-        if (Clock(CH_SNARE, 1)) {  // Use physical-only clocking
+        noise = random(0, (12 << 7) * 6) - ((12 << 7) * 3);
+
+        if (Clock(CH_SNARE, 1)) {
             env_snare.Start();
             env_snap.Start();
-            snare.Start();
         }
         if (!env_snare.GetEOC()) {
-            int32_t freq_snare = Proportion(tone_snare, BNC_MAX_PARAM, 90000) + 10000;
+            int64_t freq_snare = Proportion(tone_snare, BNC_MAX_PARAM, 600) + 20;
+            freq_snare *= 100;
 
-            // mod0 --<dirt>%--> mod1
-            int32_t freq_mod1 = snare_mod0.Next() - HEMISPHERE_3V_CV;
-            freq_mod1 = Proportion(freq_mod1, HEMISPHERE_3V_CV, FREQ_SNARE_MOD1);
-            freq_mod1 = Proportion(dirt, BNC_MAX_PARAM, freq_mod1);
-            freq_mod1 += FREQ_SNARE_MOD1;
-            snare_mod1.SetFrequency(freq_mod1);
-
-            // mod1 --30%--> snare
-            int32_t mod1 = snare_mod1.Next() - HEMISPHERE_3V_CV;
-            mod1 = Proportion(mod1, HEMISPHERE_3V_CV, (3*freq_snare)/10);
-
-            // env_snap --40%--> snare
             if (!env_snap.GetEOC()) {
-                int32_t snap = env_snap.Next();
-                snap = Proportion(snap, HEMISPHERE_3V_CV, (4*freq_snare)/10);
-                freq_snare += snap;
+                int64_t df = Proportion(env_snap.Next(), HEMISPHERE_3V_CV, freq_snare/1024);
+                df = Proportion(snap, BNC_MAX_PARAM/4, df);
+                df *= 1024;
+                freq_snare += df;
             }
+            int64_t freq_lp = freq_snare * 2;
+            int64_t freq_hp = freq_snare / 2;
 
-            freq_snare += mod1;
-            snare.SetFrequency(freq_snare);
+            int32_t snare = FilterLP(noise, freq_lp);
+            snare = FilterHP(snare, freq_hp);
+
+            // Loudness is proportional to the bandwidth, let's scale it by a
+            // factor of ~3 as we go down.
+            // bw = 40 Hz - 10 Hz = 30 Hz, bw = 640 Hz - 160 Hz = 480 Hz
+            // (40 - 10)*100/128 = 23, (640 - 160)*100/128 = 375 -> factor 16
+            // (40 - 10)*100/128 + 128 = 151, (640 - 160)*100/128 + 128 = 503 -> factor 3.3
+            int bw = freq_lp/128 - freq_hp/128;
+            snare = Proportion(375+128, bw+128, snare);
 
             levels[1] = env_snare.Next()/2;
-            sd_signal = Proportion(levels[1], HEMISPHERE_3V_CV, snare.Next());
+            sd_signal = Proportion(levels[1], HEMISPHERE_3V_CV, snare);
         }
 
         // Kick Drum Output
@@ -218,7 +206,7 @@ public:
                 SetEnvDecaySnare();
             }
             if (cursor == 6) {
-                dirt = constrain(dirt + direction, 0, BNC_MAX_PARAM);
+                snap = constrain(snap + direction, 0, BNC_MAX_PARAM);
             }
             if (cursor == 7) {
                 decay_snap = constrain(decay_snap + direction, 0, BNC_MAX_PARAM);
@@ -264,12 +252,11 @@ private:
 
     VectorOscillator env_snare;
     VectorOscillator env_snap;
-    VectorOscillator snare_mod0;
-    VectorOscillator snare_mod1;
-    VectorOscillator snare;
 
-    int noise_tone_countdown = 0;
     uint32_t noise;
+    int32_t lpf_y_prev;
+    int32_t hpf_signal_prev;
+    int32_t hpf_y_prev;
     int levels[2]; // For display
 
     // Settings
@@ -280,7 +267,7 @@ private:
 
     int tone_snare;
     int decay_snare;
-    int dirt;
+    int snap;
     int decay_snap;
     int8_t blend;
 
@@ -303,8 +290,8 @@ private:
         gfxBitmap(32, 25, 8, DECAY_ICON);
         DrawKnobAt(41, 25, 18, decay_snare, cursor == 5);
 
-        gfxBitmap(32, 35, 8, NOISE_ICON);
-        DrawKnobAt(41, 35, 18, dirt, cursor == 6);
+        gfxBitmap(32, 35, 8, FM_ICON);
+        DrawKnobAt(41, 35, 18, snap, cursor == 6);
 
         gfxBitmap(32, 45, 8, BANG_ICON);
         DrawKnobAt(41, 45, 18, decay_snap, cursor == 7);
@@ -328,11 +315,16 @@ private:
         gfxRect(x + w, y, 2, 7);
     }
 
-    void SetBDFreq() {
+    void SetFreqKick() {
         kick.SetFrequency(Proportion(tone_kick, BNC_MAX_PARAM, 3000) + 3000);
     }
     void SetEnvDecayKick() {
         env_kick.SetFrequency(1000 - Proportion(decay_kick, BNC_MAX_PARAM, 900));
+    }
+    void SetEnvDecayPunch() {
+        // 10 ms - 200 ms -> 10000 cHz - 500 cHz
+        env_punch.SetFrequency(
+            10000 - Proportion(decay_punch, BNC_MAX_PARAM, 9500));
     }
 
     void SetEnvDecaySnare() {
@@ -346,14 +338,42 @@ private:
         // }
         env_snap.SetFrequency(
             10000 - Proportion(decay_snap, BNC_MAX_PARAM, 9500));
-
     }
 
-    void SetEnvDecayPunch() {
-        // 10 ms - 200 ms -> 10000 cHz - 500 cHz
-        env_punch.SetFrequency(
-            10000 - Proportion(decay_punch, BNC_MAX_PARAM, 9500));
+    int FilterHP(int signal, int32_t cfreq){
+        // cfreq is in cHz
+        // alpha = 1/(1 + 2*pi*cfreq*dt/100)
+        // alpha = CF/(CF + cfreq)
+        // CF = 1/(2*pi*dt) for cHz
+        // sample rate dt = 60 us
+        static const int32_t CF = 265258;
+        int32_t alpha = (CF * 1024) / (CF + cfreq);
+
+        int32_t y = alpha*(hpf_y_prev + signal - hpf_signal_prev);
+        y /= 1024;
+
+        hpf_y_prev = y;
+        hpf_signal_prev = signal;
+        return y;
     }
+
+    int FilterLP(int signal, int32_t cfreq){
+        // cfreq is in cHz
+        // alpha = 2*pi*cfreq*dt/100/(1 + 2*pi*cfreq*dt/100)
+        // alpha = CF*cfreq/(1+ CF*cfreq)
+        // CF = 1/(2*pi*dt) for cHz
+        // sample rate dt = 60 us
+        static const int32_t CF = 265258;
+        int32_t alpha = (cfreq * 1024) / (CF + cfreq);
+
+
+        int32_t y = (alpha*signal) + (1024 - alpha)*lpf_y_prev;
+        y /= 1024;
+
+        lpf_y_prev = y;
+        return y;
+    }
+
 };
 
 
