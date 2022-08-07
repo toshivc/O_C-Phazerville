968fa3b130cf701c594e765276880d1379a4093a
Bryan Head <bryan.head@gmail.com>
2022-08-07 14:50:53 -0700

    wip EbbAndLfo: Add voct tracking and freq display

--- software/o_c_REV/HEM_EbbAndLfo.ino
+++ software/o_c_REV/HEM_EbbAndLfo.ino
@@ -7,22 +7,33 @@ public:
   void Start() { phase = 0; }
 
   void Controller() {
-    uint32_t phase_increment = ComputePhaseIncrement(In(0));
+    uint32_t phase_increment = ComputePhaseIncrement(pitch + In(0));
     phase += phase_increment;
-    Out(0, Proportion(phase >> 16, 1 << 16, HEMISPHERE_MAX_CV));
-  }
-  
-  void View() {
+    Out(0, Proportion(phase >> 16, 0xffff, HEMISPHERE_MAX_CV));
 
+    if (knob_accel > (1 << 8)) knob_accel--;
   }
-  
+
+  void View() {
+    gfxHeader(applet_name());
+    gfxPrintFreq(0, 18, pitch);
+    gfxPrint(0, 36, knob_accel);
+  }
+
   void SetHelp() {
 
   }
 
   void OnButtonPress() {}
 
-  void OnEncoderMove(int direction) {}
+  void OnEncoderMove(int direction) {
+    uint32_t old_pi = ComputePhaseIncrement(pitch);
+    pitch += (knob_accel >> 8) * direction;
+    while (ComputePhaseIncrement(pitch) == old_pi) {
+      pitch += direction;
+    }
+    if (knob_accel < (1 << 12)) knob_accel <<= 1;
+  }
 
   uint64_t OnDataRequest() { return 0; }
 
@@ -35,7 +46,51 @@ private:
   int dec_shape;
   int smoothness;
 
-  int phase;
+  int knob_accel = 1 << 8;
+
+  uint32_t phase;
+
+  void gfxPrintFreq(int x, int y, int16_t pitch) {
+    uint32_t num = ComputePhaseIncrement(pitch);
+    uint32_t denom = 0xffffffff / 16666;
+    bool swap = num < denom;
+    if (swap) {
+      uint32_t t = num;
+      num = denom;
+      denom = t;
+    }
+    int int_part = num / denom;
+    int digits = 0;
+    if (int_part < 10) digits = 1;
+    else if (int_part < 100) digits = 2;
+    else if (int_part < 1000) digits = 3;
+    else digits = 4;
+
+    gfxPos(x, y);
+    gfxPrint(int_part);
+    gfxPrint(".");
+
+    num %= denom;
+    while (digits < 4) {
+      num *= 10;
+      gfxPrint(num / denom);
+      num %= denom;
+      digits++;
+    }
+    if (swap) {
+      gfxPrint("s");
+    } else {
+      gfxPrint("Hz");
+    }
+    // int oom = 1;
+    // int i = -3;
+    // while (oom * phase_inc < 1000 * (uint64_t) 0xffffffff) {
+    //   oom *= 10;
+    //   i++;
+    // }
+    // gfxCursor(x, y);
+    // gfxPrint(x, y, phase_inc * oom / 0xffffffff);
+  }
 };
 ////////////////////////////////////////////////////////////////////////////////
 //// Hemisphere Applet Functions
