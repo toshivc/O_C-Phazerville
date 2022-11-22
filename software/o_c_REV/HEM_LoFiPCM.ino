945f0fc1b4575a4e2a72a9049a9b1ba568e9b89d
Nicholas J. Michalek <djphazer@gmail.com>
2022-11-22 14:50:27 -0500

    LoFi Echo: updates and refinements
    Add bitcrush, cv2 modulates rate.
    Output B plays buffer in reverse.
    Parameters are saved now.

--- software/o_c_REV/HEM_LoFiPCM.ino
+++ software/o_c_REV/HEM_LoFiPCM.ino
@@ -18,11 +18,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#define HEM_LOFI_PCM_BUFFER_SIZE 3000
+#define HEM_LOFI_PCM_BUFFER_SIZE 2048
 #define HEM_LOFI_PCM_SPEED 4
-#define LOFI_PCM2CV(S) ((uint32_t)S << 8) - 32512 //32767 is 128 << 8 32512 is 127 << 8 // 0-126 is neg, 127 is 0, 128-254 is pos
-// #define CLIPLIMIT 32512
+// #define CLIPLIMIT 6144 // 4V
 #define CLIPLIMIT HEMISPHERE_3V_CV
+
+const uint16_t CrushMask[14] = { 0x0000, 0x0001, 0x0003, 0x0007,
+                                 0x000f, 0x001f, 0x003f, 0x007f,
+                                 0x00ff, 0x01ff, 0x03ff, 0x07ff, 0x0fff, 0x1fff };
 
 class LoFiPCM : public HemisphereApplet {
 public:
@@ -33,29 +36,38 @@ public:
 
     void Start() {
         countdown = HEM_LOFI_PCM_SPEED;
-        for (int i = 0; i < HEM_LOFI_PCM_BUFFER_SIZE; i++) pcm[i] = 0; //char is unsigned in teensy (0-255)?
+        for (int i = 0; i < HEM_LOFI_PCM_BUFFER_SIZE; i++) pcm[i] = 0;
         cursor = 1; //for gui
     }
 
     void Controller() {
         play = !Gate(0); // Continuously play unless gated
-        int fdbk = Gate(1) ? 100 : feedback; // Feedback = 100 when gated
+        fdbk_g = Gate(1) ? 100 : feedback; // Feedback = 100 when gated
 
         if (play) {
-            countdown--;
-            if (countdown == 0) {
+            if (--countdown == 0) {
                 if (++head >= length) {
-                    head = 0;               
-                    ClockOut(1);
+                    head = 0;
+                    //ClockOut(1);
                 }
 
-                int dt = delaytime_pct * length / 100; //convert delaytime to length in samples 
+                int cv = In(0);
+                int cv2 = DetentedIn(1);
+
+                // bitcrush the input
+                cv = cv & (~CrushMask[depth]);
+
+                // mix input into the buffer ahead, respecting feedback
+                int dt = dt_pct * length / 100; //convert delaytime to length in samples 
                 int writehead = (head+length + dt) % length; //have to add the extra length to keep modulo positive in case delaytime is neg
-                int16_t feedbackmix = constrain((pcm[head] * fdbk / 100 + In(0)), -CLIPLIMIT, CLIPLIMIT);
+                int16_t feedbackmix = constrain((pcm[head] * fdbk_g / 100 + cv), -CLIPLIMIT, CLIPLIMIT);
                 pcm[writehead] = feedbackmix;
                 
                 Out(0, pcm[head]);
-                countdown = rate_factor;
+                Out(1, pcm[length-1 - head]); // reverse buffer!
+
+                rate_mod = constrain( rate + Proportion(cv2, HEMISPHERE_MAX_CV, 32), 1, 64);
+                countdown = rate_mod;
             }
         }
     }
@@ -67,62 +79,74 @@ public:
     }
 
     void OnButtonPress() {
-        if (++cursor > 2) cursor = 0;
+        if (++cursor > 3) cursor = 0;
         ResetCursor();
     }
 
     void OnEncoderMove(int direction) {
         switch (cursor) {
         case 0:
-            delaytime_pct = constrain(delaytime_pct += direction, 0, 99);
+            dt_pct = constrain(dt_pct += direction, 0, 99);
             break;
         case 1:
             feedback = constrain(feedback += direction, 0, 125);
             break;
         case 2:
-            rate_factor = constrain(rate_factor += direction, 1, 20);
+            rate = constrain(rate += direction, 1, 32);
+            break;
+        case 3:
+            depth = constrain(depth += direction, 0, 13);
             break;
         }
 
         //amp_offset_cv = Proportion(amp_offset_pct, 100, HEMISPHERE_MAX_CV);
         //p[cursor] = constrain(p[cursor] += direction, 0, 100);
-
-    
     }
 
     uint64_t OnDataRequest() {
         uint64_t data = 0;
+        Pack(data, PackLocation {0,7}, dt_pct);
+        Pack(data, PackLocation {7,7}, feedback);
+        Pack(data, PackLocation {14,5}, rate);
+        Pack(data, PackLocation {19,4}, depth);
         return data;
     }
 
     void OnDataReceive(uint64_t data) {
+        dt_pct = Unpack(data, PackLocation {0,7});
+        feedback = Unpack(data, PackLocation {7,7});
+        rate = Unpack(data, PackLocation {14,5});
+        depth = Unpack(data, PackLocation {19,4});
     }
 
 protected:
     void SetHelp() {
         //                               "------------------" <-- Size Guide
-        help[HEMISPHERE_HELP_DIGITALS] = "1=Mute 2=LoopMode";
-        help[HEMISPHERE_HELP_CVS]      = "1=Audio 2=";
-        help[HEMISPHERE_HELP_OUTS]     = "A=Audio B=EOC Trg";
+        help[HEMISPHERE_HELP_DIGITALS] = "1=Mute 2=Fdbk=100%";
+        help[HEMISPHERE_HELP_CVS]      = "1=Audio 2=RateMod";
+        help[HEMISPHERE_HELP_OUTS]     = "A=Audio B=Reverse";
         help[HEMISPHERE_HELP_ENCODER]  = "Time/Fdbk/Speed";
         //                               "------------------" <-- Size Guide
     }
     
 private:
+    const int length = HEM_LOFI_PCM_BUFFER_SIZE;
+
     int16_t pcm[HEM_LOFI_PCM_BUFFER_SIZE];
     bool play = 0; //play always on unless gated on Digital 1
     int head = 0; // Location of play/record head
-    int delaytime_pct = 50; //delaytime as percentage of delayline buffer
+    int dt_pct = 50; //delaytime as percentage of delayline buffer
     int feedback = 50;
+    int fdbk_g = feedback;
     int countdown = HEM_LOFI_PCM_SPEED;
-    int rate_factor = HEM_LOFI_PCM_SPEED;
-    int length = HEM_LOFI_PCM_BUFFER_SIZE;
+    int rate = HEM_LOFI_PCM_SPEED;
+    int rate_mod = rate;
+    int depth = 4; // bit depth reduction aka bitcrush
     int cursor; //for gui
-     
     
     void DrawWaveform() {
         //int inc = HEM_LOFI_PCM_BUFFER_SIZE / 1024;
-        int inc = rate_factor;
+        int inc = rate_mod;
         int disp[32];
         int high = HEMISPHERE_3V_CV;
         int pos = head - (inc * 15) - random(1,3); // Try to center the head
@@ -151,14 +175,19 @@ private:
             for (int param = 0; param < 2; param++)
             {
                 gfxIcon(31 * param, 15, param ? GAUGE_ICON : CLOCK_ICON );
-                gfxPrint(4 + pad(100, delaytime_pct), 15, delaytime_pct);
-                gfxPrint(36 + pad(1000, feedback), 15, feedback);
+                gfxPrint(4 + pad(100, dt_pct), 15, dt_pct);
+                gfxPrint(36 + pad(1000, fdbk_g), 15, fdbk_g);
                 if (param == cursor) gfxCursor(0 + (31 * param), 23, 30);
             }
         } else {
-            gfxPrint(0, 15, "Rate:");
-            gfxPrint(30, 15, rate_factor);
-            gfxCursor(30, 23, 16);
+            //gfxPrint(0, 15, "XxX");
+            gfxIcon(0, 15, WAVEFORM_ICON);
+            gfxIcon(8, 15, BURST_ICON);
+            gfxIcon(22, 15, LEFT_RIGHT_ICON);
+            gfxPrint(30, 15, rate_mod);
+            gfxIcon(42, 15, UP_DOWN_ICON);
+            gfxPrint(50, 15, depth);
+            gfxCursor(30 + (cursor-2)*20, 23, 14);
         }
     }
     
