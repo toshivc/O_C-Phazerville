e82a43c218f8b9f256dbe841bd2ea810ed05a245
Nicholas J. Michalek <djphazer@gmail.com>
2022-12-08 04:38:33 -0500

    Re-enable a bunch of stuff, version bump to 1.4.4-pio
    Platform IO build for testing. Fills only 77% of flash.

--- software/o_c_REV/hemisphere_config.h
+++ software/o_c_REV/hemisphere_config.h
@@ -11,7 +11,7 @@
 // * Category filtering is deprecated at 1.8, but I'm leaving the per-applet categorization
 // alone to avoid breaking forked codebases by other developers.
 
-#define HEMISPHERE_AVAILABLE_APPLETS 55
+#define HEMISPHERE_AVAILABLE_APPLETS 59
 
 //////////////////  id  cat   class name
 #define HEMISPHERE_APPLETS { \
@@ -35,18 +35,22 @@
     DECLARE_APPLET( 57, 0x02, DrumMap), \
     DECLARE_APPLET(  9, 0x08, DualQuant), \
     DECLARE_APPLET( 18, 0x02, DualTM), \
-    DECLARE_APPLET( 42, 0x11, EnvFollow), \
     DECLARE_APPLET( 63, 0x06, EbbAndLfo), \
+    DECLARE_APPLET( 45, 0x02, EnigmaJr), \
+    DECLARE_APPLET( 42, 0x11, EnvFollow), \
     DECLARE_APPLET( 29, 0x04, GateDelay), \
+    DECLARE_APPLET( 17, 0x50, GatedVCA), \
     DECLARE_APPLET( 16, 0x80, LoFiPCM), \
     DECLARE_APPLET( 10, 0x44, Logic), \
     DECLARE_APPLET( 21, 0x01, LowerRenz), \
+    DECLARE_APPLET(150, 0x20, hMIDIIn), \
+    DECLARE_APPLET( 27, 0x20, hMIDIOut), \
     DECLARE_APPLET( 50, 0x04, Metronome), \
     DECLARE_APPLET( 33, 0x10, MixerBal), \
     DECLARE_APPLET( 20, 0x02, Palimpsest), \
     DECLARE_APPLET( 59, 0x04, ProbabilityDivider), \
     DECLARE_APPLET( 62, 0x04, ProbabilityMelody), \
-    DECLARE_APPLET( 45, 0x01, RndWalk), \
+    DECLARE_APPLET( 69, 0x01, RndWalk), \
     DECLARE_APPLET( 44, 0x01, RunglBook), \
     DECLARE_APPLET( 26, 0x08, ScaleDuet), \
     DECLARE_APPLET( 40, 0x40, Schmitt), \
@@ -73,10 +77,5 @@
 }
 /*
     DECLARE_APPLET(127, 0x80, DIAGNOSTIC), \
-    DECLARE_APPLET(150, 0x20, hMIDIIn), \
-    DECLARE_APPLET( 27, 0x20, hMIDIOut), \
-    DECLARE_APPLET( 45, 0x02, EnigmaJr), \
-    DECLARE_APPLET( 17, 0x50, GatedVCA), \
-    DECLARE_APPLET( 55, 0x80, DrCrusher), \
     DECLARE_APPLET(  7, 0x01, SkewedLFO), \
 */
