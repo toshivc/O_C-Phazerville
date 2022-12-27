2f508a19dea9698f21fb617a7ffecfb106c174d8
Nicholas J. Michalek <djphazer@gmail.com>
2022-12-27 03:15:08 -0500

    Version bump 1.4.7-pio

--- software/o_c_REV/OC_version.h
+++ software/o_c_REV/OC_version.h
@@ -1,6 +1,6 @@
 #ifndef OC_VERSION_H_
 #define OC_VERSION_H_
 #define OC_VERSION_TITLE "Phazerville Suite"
-#define OC_VERSION "v1.4.6-pio"
+#define OC_VERSION "v1.4.7-pio"
 #define OC_VERSION_URL "github.com/djphazer"
 #endif
