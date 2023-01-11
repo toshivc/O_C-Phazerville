cfee7364a7cec1645f2f9243f657abb7ec28df4e
Nicholas J. Michalek <djphazer@gmail.com>
2023-01-11 01:21:00 -0500

    Version bump v1.4.9

--- software/o_c_REV/OC_version.h
+++ software/o_c_REV/OC_version.h
@@ -1,6 +1,6 @@
 #ifndef OC_VERSION_H_
 #define OC_VERSION_H_
 #define OC_VERSION_TITLE "Phazerville Suite"
-#define OC_VERSION "v1.4.8.2"
+#define OC_VERSION "v1.4.9"
 #define OC_VERSION_URL "github.com/djphazer"
 #endif
