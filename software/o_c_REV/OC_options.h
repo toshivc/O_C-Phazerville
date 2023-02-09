fe2c9d62a8b89f2bef604aab86ed505426b5e347
Ben Rosenbach <ben@benrosenbach.com>
2023-02-09 11:27:50 -0500

    Add all of the full width apps back in

--- software/o_c_REV/OC_options.h
+++ software/o_c_REV/OC_options.h
@@ -27,10 +27,10 @@
 /* Flags for the full-width apps, these enable/disable them in OC_apps.ino but also zero out the app   */
 /* files to prevent them from taking up space. Only Enigma is enabled by default.                      */
 #define ENABLE_APP_ENIGMA
-//#define ENABLE_APP_MIDI
-//#define ENABLE_APP_NEURAL_NETWORK
-//#define ENABLE_APP_PONG
-//#define ENABLE_APP_DARKEST_TIMELINE
+#define ENABLE_APP_MIDI
+#define ENABLE_APP_NEURAL_NETWORK
+#define ENABLE_APP_PONG
+#define ENABLE_APP_DARKEST_TIMELINE
 
 
 #endif
