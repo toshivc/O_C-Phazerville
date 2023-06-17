d816d35897dec785ba4ac2fec7cae4292e606c5d
Nicholas J. Michalek <djphazer@gmail.com>
2023-06-17 02:49:43 -0400

    import ResetClock applet from pkyme
    adapted for 64-bit save data and modal cursor

--- software/o_c_REV/hemisphere_config.h
+++ software/o_c_REV/hemisphere_config.h
@@ -51,6 +51,7 @@
     DECLARE_APPLET( 20, 0x02, Palimpsest), \
     DECLARE_APPLET( 59, 0x04, ProbabilityDivider), \
     DECLARE_APPLET( 62, 0x04, ProbabilityMelody), \
+    DECLARE_APPLET( 70, 0x14, ResetClock), \
     DECLARE_APPLET( 69, 0x01, RndWalk), \
     DECLARE_APPLET( 44, 0x01, RunglBook), \
     DECLARE_APPLET( 26, 0x08, ScaleDuet), \
