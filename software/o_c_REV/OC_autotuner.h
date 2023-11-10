c1cb32da5bc046d7649590882de20b323993be6f
Nicholas J. Michalek <djphazer@gmail.com>
2023-11-09 22:51:47 -0500

    Autotuner: fixes/refactoring; succeed when ceiling reached

--- software/o_c_REV/OC_autotuner.h
+++ software/o_c_REV/OC_autotuner.h
@@ -13,7 +13,9 @@ static constexpr int ACTIVE_OCTAVES = OCTAVES - 1;
 #endif
 
 static constexpr size_t kHistoryDepth = 10;
-  
+
+#define ZERO_OFFSET (5 - OC::DAC::kOctaveZero)
+
 #define FREQ_MEASURE_TIMEOUT 512
 #define ERROR_TIMEOUT (FREQ_MEASURE_TIMEOUT << 0x4)
 #define MAX_NUM_PASSES 1500
@@ -21,19 +23,12 @@ static constexpr size_t kHistoryDepth = 10;
 
 #if defined(BUCHLA_4U) && !defined(IO_10V)
 const char* const AT_steps[] = {
+  " ", " ", " ", " ", " ", // 5 blanks
   "0.0V", "1.2V", "2.4V", "3.6V", "4.8V", "6.0V", "7.2V", "8.4V", "9.6V", "10.8V", " " 
-};
-#elif defined(IO_10V)
-const char* const AT_steps[] = {
-  "0.0V", "1.0V", "2.0V", "3.0V", "4.0V", "5.0V", "6.0V", "7.0V", "8.0V", "9.0V", " " 
-};
-#elif defined(VOR)
-const char* const AT_steps[] = {
-  "0.0V", "1.0V", "2.0V", "3.0V", "4.0V", "5.0V", "6.0V", "7.0V", "8.0V", "9.0V", "10.0V", " " 
 };
 #else
 const char* const AT_steps[] = {
-  "-3V", "-2V", "-1V", " 0V", "+1V", "+2V", "+3V", "+4V", "+5V", "+6V", " " 
+  "-5V", "-4V", "-3V", "-2V", "-1V", " 0V", "+1V", "+2V", "+3V", "+4V", "+5V", "+6V", "+7V", "+8V", "+9V", "+10V", " " 
 };
 #endif
 
@@ -124,20 +119,19 @@ public:
     data_select_ = 0;
     channel_ = DAC_CHANNEL_A;
     calibration_data_ = 0;
-    auto_tune_running_status_ = AT_OFF;
+    run_status_ = AT_OFF;
 
     // moved from REFS
     armed_ = false;
-    autotuner_step_ = OC::DAC_VOLT_0_ARM;
+    step_ = OC::DAC_VOLT_0_ARM;
     auto_DAC_offset_error_ = 0;
     auto_frequency_ = 0;
     auto_last_frequency_ = 0;
     auto_freq_sum_ = 0;
     auto_freq_count_ = 0;
-    auto_ready_ = 0;
+    ready_ = 0;
     ticks_since_last_freq_ = 0;
-    auto_next_step_ = false;
-    autotune_completed_ = false;
+    completed_ = false;
     F_correction_factor_ = 0xFF;
     correction_direction_ = false;
     correction_cnt_positive_ = 0x0;
@@ -174,22 +168,21 @@ private:
   size_t data_select_;
   DAC_CHANNEL channel_;
   uint8_t calibration_data_;
-  AT_STATUS auto_tune_running_status_;
+  AT_STATUS run_status_;
 
   // moved from References
   OC::vfx::ScrollingHistory<uint32_t, kHistoryDepth> history_;
 
   bool armed_;
-  uint8_t autotuner_step_;
+  uint8_t step_;
   int32_t auto_DAC_offset_error_;
   uint32_t auto_frequency_;
   uint32_t auto_target_frequencies_[OCTAVES + 1];
   int16_t auto_calibration_data_[OCTAVES + 1];
   uint32_t auto_last_frequency_;
-  bool auto_next_step_;
-  bool auto_error_;
-  bool auto_ready_;
-  bool autotune_completed_;
+  bool error_;
+  bool ready_;
+  bool completed_;
   uint32_t auto_freq_sum_;
   uint32_t auto_freq_count_;
   uint32_t ticks_since_last_freq_;
@@ -209,18 +202,6 @@ private:
   void handleButtonDown(const UI::Event &event);
 
 
-  void autotuner_reset_completed() {
-    autotune_completed_ = false;
-  }
-
-  inline uint8_t get_octave_cnt() {
-    return octaves_cnt_ + 0x1;
-  }
-
-  inline uint8_t auto_tune_step() {
-    return autotuner_step_;
-  }
-
   void autotuner_arm(uint8_t _status) {
     reset_autotuner();
     armed_ = _status ? true : false;
@@ -228,30 +209,32 @@ private:
   
   void autotuner_run() {     
     SERIAL_PRINTLN("Starting autotuner...");
-    autotuner_step_ = armed_ ? OC::DAC_VOLT_0_BASELINE : OC::DAC_VOLT_0_ARM;
-    if (autotuner_step_ == OC::DAC_VOLT_0_BASELINE)
+    step_ = armed_ ? OC::DAC_VOLT_0_BASELINE : OC::DAC_VOLT_0_ARM;
+    if (step_ == OC::DAC_VOLT_0_BASELINE)
     // we start, so reset data to defaults:
       OC::DAC::set_default_channel_calibration_data(channel_);
   }
 
-  void auto_reset_step() {
+  void auto_next_step() {
     SERIAL_PRINTLN("Autotuner reset step...");
+    ticks_since_last_freq_ = 0x0;
     auto_num_passes_ = 0x0;
     auto_DAC_offset_error_ = 0x0;
     correction_direction_ = false;
     correction_cnt_positive_ = correction_cnt_negative_ = 0x0;
     F_correction_factor_ = 0xFF;
-    auto_ready_ = false;
+    ready_ = false;
+    step_++;
   }
 
   void reset_autotuner() {
     ticks_since_last_freq_ = 0x0;
     auto_frequency_ = 0x0;
     auto_last_frequency_ = 0x0;
-    auto_error_ = 0x0;
-    auto_ready_ = 0x0;
+    error_ = 0x0;
+    ready_ = 0x0;
     armed_ = 0x0;
-    autotuner_step_ = 0x0;
+    step_ = 0x0;
     F_correction_factor_ = 0xFF;
     correction_direction_ = false;
     correction_cnt_positive_ = 0x0;
@@ -259,16 +242,8 @@ private:
     octaves_cnt_ = 0x0;
     auto_num_passes_ = 0x0;
     auto_DAC_offset_error_ = 0x0;
-    autotune_completed_ = 0x0;
+    completed_ = 0x0;
     reset_calibration_data();
-  }
-
-  const uint32_t get_auto_frequency() {
-    return auto_frequency_;
-  }
-
-  uint8_t _ready() {
-     return auto_ready_;
   }
 
   void reset_calibration_data() {
@@ -296,7 +271,7 @@ private:
     bool _f_result = false;
 
     if (ticks_since_last_freq_ > ERROR_TIMEOUT) {
-      auto_error_ = true;
+      error_ = true;
     }
     
     if (FreqMeasure.available()) {
@@ -313,7 +288,7 @@ private:
         auto_frequency_ = uint32_t(FreqMeasure.countToFrequency(auto_freq_sum_ / auto_freq_count_) * 1000);
         history_.Push(auto_frequency_);
         auto_freq_sum_ = 0;
-        auto_ready_ = true;
+        ready_ = true;
         auto_freq_count_ = 0;
         _f_result = true;
         ticks_since_last_freq_ = 0x0;
@@ -326,7 +301,7 @@ private:
 
   void measure_frequency_and_calc_error() {
 
-    switch(autotuner_step_) {
+    switch(step_) {
 
       case OC::DAC_VOLT_0_ARM:
       case OC::DAC_VOLT_WAIT:
@@ -349,10 +324,9 @@ private:
           auto_frequency_ = ((auto_frequency_ + average) / (kHistoryDepth + 1)); // 0V 
           SERIAL_PRINTLN("Baseline auto_frequency_ = %4.d", auto_frequency_);
           // reset step, and proceed:
-          auto_reset_step();
-          autotuner_step_++;
+          auto_next_step();
           // wait for user to patch output to oscillator V/Oct
-          auto_tune_running_status_ = AT_WAIT;
+          run_status_ = AT_WAIT;
         }
         else if (_update) 
           auto_num_passes_++;
@@ -375,13 +349,13 @@ private:
             break;
           }
         #else
-          auto_target_frequencies_[octaves_cnt_]  =  auto_frequency_ * target_multipliers[octaves_cnt_ + (5 - OC::DAC::kOctaveZero)]; 
+          auto_target_frequencies_[octaves_cnt_]  =  auto_frequency_ * target_multipliers[octaves_cnt_ + ZERO_OFFSET]; 
         #endif
         octaves_cnt_++;
         // go to next step, if done:
         if (octaves_cnt_ > ACTIVE_OCTAVES) {
           octaves_cnt_ = 0x0;
-          autotuner_step_++;
+          step_++;
         }
       }
       break;
@@ -405,8 +379,11 @@ private:
           /* target frequency reached */
           SERIAL_PRINTLN("* Target Frequency Reached *");
           
-          if ((autotuner_step_ > OC::DAC_VOLT_2m) && (auto_last_frequency_ * 1.25f > auto_frequency_))
-              auto_error_ = true; // throw error, if things don't seem to double ...
+          int step_idx = step_ - OC::DAC_VOLT_3m;
+
+          // if things don't seem to double ... we've hit the ceiling.
+          if ((step_ > OC::DAC_VOLT_2m) && (auto_last_frequency_ * 1.25f > auto_frequency_))
+              step_ = OC::AUTO_CALIBRATION_STEP_LAST - 1;
 
           // average:
           uint32_t history[kHistoryDepth]; 
@@ -418,22 +395,21 @@ private:
           // store last frequency:
           auto_last_frequency_  = (auto_frequency_ + average) / (kHistoryDepth + 1);
           // and DAC correction value:
-          auto_calibration_data_[autotuner_step_ - OC::DAC_VOLT_3m] = auto_DAC_offset_error_;
+          auto_calibration_data_[step_idx] = auto_DAC_offset_error_;
 
           // reset and step forward
-          auto_reset_step();
-          autotuner_step_++; 
+          auto_next_step();
         }
         else if (_update)
         {
           auto_num_passes_++; // count passes
 
-          SERIAL_PRINTLN("auto_target_frequencies[%3d]_ = %3d", autotuner_step_ - OC::DAC_VOLT_3m,
-                        auto_target_frequencies_[autotuner_step_ - OC::DAC_VOLT_3m] );
+          SERIAL_PRINTLN("auto_target_frequencies[%3d]_ = %3d", step_ - OC::DAC_VOLT_3m,
+                        auto_target_frequencies_[step_ - OC::DAC_VOLT_3m] );
           SERIAL_PRINTLN("auto_frequency_ = %3d", auto_frequency_);
 
           // and correct frequency
-          if (auto_target_frequencies_[autotuner_step_ - OC::DAC_VOLT_3m] > auto_frequency_)
+          if (auto_target_frequencies_[step_ - OC::DAC_VOLT_3m] > auto_frequency_)
           {
             // update correction factor?
             if (!correction_direction_)
@@ -446,7 +422,7 @@ private:
             // we're converging -- count passes, so we can stop after x attempts:
             if (F_correction_factor_ == 0x1) correction_cnt_positive_++;
           }
-          else if (auto_target_frequencies_[autotuner_step_ - OC::DAC_VOLT_3m] < auto_frequency_)
+          else if (auto_target_frequencies_[step_ - OC::DAC_VOLT_3m] < auto_frequency_)
           {
             // update correction factor?
             if (correction_direction_)
@@ -474,8 +450,8 @@ private:
         int32_t new_auto_calibration_point = OC::calibration_data.dac.calibrated_octaves[channel_][octaves_cnt_] + auto_calibration_data_[octaves_cnt_];
         // write to DAC and update data
         if (new_auto_calibration_point >= 65536 || new_auto_calibration_point < 0) {
-          auto_error_ = true;
-          autotuner_step_++;
+          error_ = true;
+          step_++;
         }
         else {
           OC::DAC::set(channel_, new_auto_calibration_point);
@@ -486,22 +462,22 @@ private:
       }
       // then stop ... 
       if (octaves_cnt_ > OCTAVES) { 
-        autotune_completed_ = true;
+        completed_ = true;
         // and point to auto data ...
         OC::DAC::set_auto_channel_calibration_data(channel_);
-        autotuner_step_++;
+        step_++;
       }
       break;
       default:
-      autotuner_step_ = OC::DAC_VOLT_0_ARM;
+      step_ = OC::DAC_VOLT_0_ARM;
       armed_ = 0x0;
       break;
     }
   }
   
-  void autotune_updateDAC() {
+  void updateDAC() {
 
-    switch(autotuner_step_) {
+    switch(step_) {
 
       case OC::DAC_VOLT_0_ARM: 
       {
@@ -522,7 +498,7 @@ private:
       default: 
       // set DAC to calibration point + error
       {
-        int32_t _default_calibration_point = OC::calibration_data.dac.calibrated_octaves[channel_][autotuner_step_ - OC::DAC_VOLT_3m];
+        int32_t _default_calibration_point = OC::calibration_data.dac.calibrated_octaves[channel_][step_ - OC::DAC_VOLT_3m];
         OC::DAC::set(channel_, _default_calibration_point + auto_DAC_offset_error_);
       }
       break;
@@ -533,18 +509,18 @@ private:
   template <typename Owner>
   void Autotuner<Owner>::ISR() {
       if (armed_) {
-          autotune_updateDAC();
+          updateDAC();
           measure_frequency_and_calc_error();
           ticks_since_last_freq_++;
       }
 
-    if (auto_error_) {
-      auto_tune_running_status_ = AT_ERROR;
+    if (error_) {
+      run_status_ = AT_ERROR;
       reset_autotuner();
     }
-    else if (autotune_completed_) {
-      auto_tune_running_status_ = AT_DONE;
-      autotuner_reset_completed();
+    else if (completed_) {
+      run_status_ = AT_DONE;
+      completed_ = false;
     }
     
   }
@@ -557,7 +533,7 @@ private:
     weegfx::coord_t y = 0;
     weegfx::coord_t h = 64;
 
-    graphics.clearRect(x, y, w, h);
+    //graphics.clearRect(x, y, w, h);
     graphics.drawFrame(x, y, w, h);
     graphics.setPrintPos(x + 2, y + 3);
     graphics.print(OC::Strings::channel_id[channel_]);
@@ -585,7 +561,7 @@ private:
       }
       else if (i == AUTOTUNE) {
         
-        switch (auto_tune_running_status_) {
+        switch (run_status_) {
         //to display progress, if running
         case AT_OFF:
         graphics.print("run --> ");
@@ -593,14 +569,14 @@ private:
         break;
         case AT_READY: {
         graphics.print("arm > ");
-        const uint32_t _freq = get_auto_frequency();
+        const uint32_t _freq = auto_frequency_;
         if (_freq == 0)
-          graphics.printf("wait ...");
+          graphics.print("wait ...");
         else 
         {
           const uint32_t value = _freq / 1000;
           const uint32_t cents = _freq % 1000;
-          graphics.printf("%5u.%03u", value, cents);
+          graphics.printf("%4u.%03u", value, cents);
         }
         }
         break;
@@ -610,23 +586,23 @@ private:
         break;
         case AT_RUN:
         {
-          int _octave = get_octave_cnt();
+          int _octave = octaves_cnt_ + 0x1;
           if (_octave > 1 && _octave < OCTAVES) {
             for (int i = 0; i <= _octave; i++, x += 6)
               graphics.drawBitmap8(x + 18, y + 4, 4, OC::bitmap_indicator_4x8);
           }
-          else if (auto_tune_step() == DAC_VOLT_0_BASELINE || auto_tune_step() == DAC_VOLT_TARGET_FREQUENCIES) // this goes too quick, so ... 
+          else if (step_ == DAC_VOLT_0_BASELINE || step_ == DAC_VOLT_TARGET_FREQUENCIES) // this goes too quick, so ... 
             graphics.print(" 0.0V baseline");
           else {
-            graphics.print(AT_steps[auto_tune_step() - DAC_VOLT_3m]);
-            if (!_ready())
+            graphics.print(AT_steps[step_ - DAC_VOLT_3m + ZERO_OFFSET]);
+            if (!ready_)
               graphics.print(" ");
             else 
             {
-              const uint32_t f = get_auto_frequency();
+              const uint32_t f = auto_frequency_;
               const uint32_t value = f / 1000;
               const uint32_t cents = f % 1000;
-              graphics.printf(" > %5u.%03u", value, cents);
+              graphics.printf(" > %4u.%03u", value, cents);
             }
           }
         }
@@ -712,16 +688,16 @@ private:
   template <typename Owner>
   void Autotuner<Owner>::move_cursor(int offset) {
 
-    if (auto_tune_running_status_ < AT_RUN) {
+    if (run_status_ < AT_RUN) {
       int cursor_pos = cursor_pos_ + offset;
       CONSTRAIN(cursor_pos, 0, AUTO_MENU_ITEMS_LAST - 0x2);  
       cursor_pos_ = cursor_pos;
       //
       if (cursor_pos_ == DATA_SELECT)
-          auto_tune_running_status_ = AT_OFF;
+          run_status_ = AT_OFF;
     }
-    else if (auto_tune_running_status_ == AT_ERROR || auto_tune_running_status_ == AT_DONE)
-      auto_tune_running_status_ = AT_OFF;
+    else if (run_status_ == AT_ERROR || run_status_ == AT_DONE)
+      run_status_ = AT_OFF;
   }
 
   template <typename Owner>
@@ -752,14 +728,14 @@ private:
       break;
       case AUTOTUNE: 
       {
-        if (auto_tune_running_status_ < AT_RUN) {
-          int _status = auto_tune_running_status_ + offset;
+        if (run_status_ < AT_RUN) {
+          int _status = run_status_ + offset;
           CONSTRAIN(_status, 0, AT_READY);
-          auto_tune_running_status_ = AT_STATUS(_status);
+          run_status_ = AT_STATUS(_status);
           autotuner_arm(_status);
         }
-        else if (auto_tune_running_status_ == AT_ERROR || auto_tune_running_status_ == AT_DONE)
-          auto_tune_running_status_ = AT_OFF;
+        else if (run_status_ == AT_ERROR || run_status_ == AT_DONE)
+          run_status_ = AT_OFF;
       }
       break;
       default:
@@ -770,33 +746,33 @@ private:
   template <typename Owner>
   void Autotuner<Owner>::handleButtonUp(const UI::Event &event) {
 
-    if (cursor_pos_ == AUTOTUNE && auto_tune_running_status_ == AT_OFF) {
+    if (cursor_pos_ == AUTOTUNE && run_status_ == AT_OFF) {
       // arm the tuner
-      auto_tune_running_status_ = AT_READY;
-      autotuner_arm(auto_tune_running_status_);
+      run_status_ = AT_READY;
+      autotuner_arm(run_status_);
     }
     else {
       reset_autotuner();
-      auto_tune_running_status_ = AT_OFF;
+      run_status_ = AT_OFF;
     }
   }
   
   template <typename Owner>
   void Autotuner<Owner>::handleButtonDown(const UI::Event &event) {
-    if (auto_tune_running_status_ == AT_ERROR) {
+    if (run_status_ == AT_ERROR) {
       reset_autotuner();
-      auto_tune_running_status_ = AT_OFF;
+      run_status_ = AT_OFF;
       return;
     }  
     
     if (cursor_pos_ == AUTOTUNE) {
-        if (auto_tune_running_status_ == AT_READY) {
+        if (run_status_ == AT_READY) {
           autotuner_run();
-          auto_tune_running_status_ = AT_RUN;
+          run_status_ = AT_RUN;
         }
-        else if (auto_tune_running_status_ == AT_WAIT) {
-          auto_tune_running_status_ = AT_RUN;
-          autotuner_step_++;
+        else if (run_status_ == AT_WAIT) {
+          auto_next_step();
+          run_status_ = AT_RUN;
         }
     }
   }
@@ -816,7 +792,7 @@ private:
     else
       data_select_ = 0x00;
     cursor_pos_ = 0x0;
-    auto_tune_running_status_ = AT_OFF;
+    run_status_ = AT_OFF;
   }
   
   template <typename Owner>
