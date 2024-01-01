6ab5c62c7407ccdf6bfa3e1068fcc676b9c616eb
Nicholas J. Michalek <djphazer@gmail.com>
2024-01-01 18:54:39 -0500

    Fix default button mapping for Teensy 4.1
    TODO: make FLIP_180 a runtime check / calibration option

--- software/o_c_REV/OC_gpio.cpp
+++ software/o_c_REV/OC_gpio.cpp
@@ -14,7 +14,7 @@ uint8_t TR1=0, TR2=1, TR3=2, TR4=3;
 uint8_t OLED_DC=6, OLED_RST=7, OLED_CS=8;
 uint8_t DAC_CS=10, DAC_RST=9;
 uint8_t encL1=22, encL2=21, butL=23, encR1=16, encR2=15, butR=14;
-uint8_t but_top=4, but_bot=5, but_mid=255, but_top2=255, but_bot2=255;
+uint8_t but_top=5, but_bot=4, but_mid=255, but_top2=255, but_bot2=255;
 uint8_t OC_GPIO_DEBUG_PIN1=24, OC_GPIO_DEBUG_PIN2=25;
 bool ADC33131D_Uses_FlexIO=false;
 bool OLED_Uses_SPI1=false;
