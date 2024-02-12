5d1a16e26de3507d5fec0bdef767caca76d977ec
Nicholas J. Michalek <djphazer@gmail.com>
2024-02-11 19:41:02 -0500

    build flags in OC_options.h for Arduino IDE

--- software/o_c_REV/OC_options.h
+++ software/o_c_REV/OC_options.h
@@ -3,6 +3,8 @@
  * compile options.
  *
  */
+// Edit this file to customize when compiling with Arduino IDE
+// Sets of these flags are also defined in platformio.ini
 
 #ifndef OC_OPTIONS_H_
 #define OC_OPTIONS_H_
@@ -25,9 +27,7 @@
 //#define DAC8564
 
 
-// Phazerville Suite includes full support for the Plum Audio hardware variants with VOR
-// The VOR flag is set in platformio.ini.
-/* ------------ uncomment for use with Plum Audio VOR anabled versions (OCP, 1uO_c v2, 4Robots) ---------------------------------------------------------  */
+/* ------------ uncomment for use with Plum Audio VOR enabled versions (OCP, 1uO_c v2, 4Robots) -----  */
 //#define VOR
 
 // idk what this means so I'm keeping it -NJM
@@ -35,23 +35,38 @@
   #define IO_10V
 #endif
 
+// Here are some custom flags:
+/* --- special Phazerville mode w/ easter eggs --- */
+// #define PEWPEWPEW
+/* --- alternate Grids patterns for DrumMap applet --- */
+// #define DRUMMAP_GRIDS2
+// 16 presets in Hemisphere
+// #define MOAR_PRESETS
+
 /* Flags for the full-width apps, these enable/disable them in OC_apps.ino but also zero out the app   */
 /* files to prevent them from taking up space.                                                         */
-// Sets of these are now defined in platformio.ini for convenience. -NJM
 
-// Hemisphere apps
+// #define ENABLE_APP_CALIBR8OR
+// #define ENABLE_APP_SCENES
 // #define ENABLE_APP_ENIGMA
 // #define ENABLE_APP_MIDI
-// #define ENABLE_APP_NEURAL_NETWORK
 // #define ENABLE_APP_PONG
+// #define ENABLE_APP_PIQUED
+// #define ENABLE_APP_POLYLFO
+// #define ENABLE_APP_H1200
+// #define ENABLE_APP_BYTEBEATGEN
+// #define ENABLE_APP_NEURAL_NETWORK
 // #define ENABLE_APP_DARKEST_TIMELINE
-
-// Stock O&C apps
+// #define ENABLE_APP_LORENZ
+// #define ENABLE_APP_ASR
 // #define ENABLE_APP_QUANTERMAIN
 // #define ENABLE_APP_METAQ
 // #define ENABLE_APP_CHORDS
+// #define ENABLE_APP_PASSENCORE
 // #define ENABLE_APP_SEQUINS
-
+// #define ENABLE_APP_AUTOMATONNETZ
+// #define ENABLE_APP_BBGEN
+// #define ENABLE_APP_REFERENCES
 
 #endif
 
