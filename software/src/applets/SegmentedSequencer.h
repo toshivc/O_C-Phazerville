6d4ce923b37dfe0aeabcfbb4503f8a1a9d9dc418
toshivc <toshivc@gmail.com>
2024-08-19 23:46:37 +1000

    changed UI to fit on disp.

--- software/src/applets/SegmentedSequencer.h
+++ software/src/applets/SegmentedSequencer.h
@@ -7,6 +7,11 @@ public:
         return "SegSeq";
     }
 
+    enum SegSeqCursor {
+      NUM_SEGMENT, RANDOMIZE, KEY, SCALE, PITCH_RANGE, DENSITY, TIME,
+      DURATION,
+    };
+
     void Start() {
         num_segments = 4;  // Default to 4 segments
         current_segment = 0;
@@ -39,30 +44,37 @@ public:
         DrawInterface();
     }
 
-    void OnButtonPress() {
-        if (cursor == 0) {
+    /*void OnButtonPress() {
+        if (cursor == NUM_SEGMENT) {
             RandomizeAllSegments();
         } else {
-            RandomizeSegment(current_segment);
+            //RandomizeSegment(current_segment);
         }
-    }
+    }*/
 
     void OnEncoderMove(int direction) {
-        if (cursor == 0) {
-            num_segments = constrain(num_segments + direction, 1, SEQ_MAX_SEGMENTS);
-        } else {
-            Segment& seg = segments[current_segment];
-            switch(cursor) {
-                case 1: seg.scale = constrain(seg.scale + direction, 0, 15); break;
-                case 2: seg.key = constrain(seg.key + direction, 0, 11); break;
-                case 3: seg.density = constrain(seg.density + direction, 0, 14); break;
-                case 4: seg.time_signature = constrain(seg.time_signature + direction, 0, 3); break;
-                case 5: seg.duration = constrain(seg.duration + direction, 0, 14); break;
-                case 6: seg.pitch_range = constrain(seg.pitch_range + direction, 0, 14); break;
-            }
-            GenerateSegment(current_segment);
+        if (!EditMode()) { // move cursor
+        MoveCursor(cursor, direction, 7);
+        return;
         }
+
+
+        Segment& seg = segments[current_segment];
+        switch(cursor) {
+            case NUM_SEGMENT: num_segments = constrain(num_segments + direction, 1, SEQ_MAX_SEGMENTS); break;
+            case RANDOMIZE: 
+            if(direction>1){RandomizeAllSegments();}else{RandomizeSegment(current_segment);}
+            case KEY: seg.key = constrain(seg.key + direction, 0, 11); break;
+            case SCALE: seg.scale = constrain(seg.scale + direction, 0, 15); break;
+            case PITCH_RANGE: seg.pitch_range = constrain(seg.pitch_range + direction, 0, 14); break;
+            case DENSITY: seg.density = constrain(seg.density + direction, 0, 14); break;
+            case TIME: seg.time_signature = constrain(seg.time_signature + direction, 0, 3); break;
+            case DURATION: seg.duration = constrain(seg.duration + direction, 0, 14); break;
+            
+        }
+        GenerateSegment(current_segment);
     }
+ 
 
     uint64_t OnDataRequest() {
         uint64_t data = 0;
@@ -96,9 +108,10 @@ private:
 
     Segment segments[SEQ_MAX_SEGMENTS];
     uint8_t num_segments;
-    uint8_t current_segment;
+    uint8_t current_segment;       //segment that is playing
+    uint8_t edit_segment;       //segment that is being edited
     uint32_t step_counter;
-    uint8_t cursor;
+    int cursor;
 
     uint8_t notes[SEQ_MAX_STEPS];
     uint32_t gates;
@@ -161,34 +174,44 @@ private:
 
     void DrawInterface() {
         gfxHeader("SegSeq");
+
         gfxPrint(1, 15, "Segs:");
         gfxPrint(30, 15, num_segments);
+        gfxIcon(43,13, RANDOM_ICON);
         
-        gfxPrint(1, 25, "Seg ");
+        gfxPrint(1, 25, "S ");
         gfxPrint(current_segment + 1);
         gfxPrint(":");
+        gfxPrint(OC::Strings::note_names_unpadded[segments[current_segment].key]);
+        gfxPrint(38, 25, "");
         gfxPrint(OC::scale_names_short[segments[current_segment].scale]);
         
-        gfxPrint(1, 35, "Key:");
-        gfxPrint(OC::Strings::note_names_unpadded[segments[current_segment].key]);
+        gfxPrint(1, 35, "Rng:");
+        gfxPrint(segments[current_segment].pitch_range);
         
         gfxPrint(1, 45, "Den:");
         gfxPrint(segments[current_segment].density);
         
-        gfxPrint(1, 55, "Tim:");
         const char* time_sigs[] = {"2/4", "3/4", "4/4", "5/4"};
-        gfxPrint(time_sigs[segments[current_segment].time_signature]);
+        gfxPrint(1, 55, time_sigs[segments[current_segment].time_signature]);
+        gfxPrint(24, 55, "Dur:");
+        gfxPrint(segments[current_segment].duration);
 
-        // Draw cursor
-        if (cursor > 0) {
-            gfxCursor(1, 25 + (cursor - 1) * 10, 62);
-        } else {
-            gfxCursor(28, 15, 12);
-        }
+
+       switch (cursor){
+        case NUM_SEGMENT: gfxCursor(28, 23, 12); break;
+        case RANDOMIZE: gfxCursor(41,23,11); break;
+        case KEY: gfxCursor(24, 33, 12); break;
+        case SCALE: gfxCursor(38, 33, 24); break;
+        case PITCH_RANGE: gfxCursor(24,43,12); break;
+        case DENSITY: gfxCursor(24, 53, 12); break;
+        case TIME: gfxCursor(1, 63, 20); break;
+        case DURATION: gfxCursor(46,63,12); break;
+       }
     }
 
     int GetSegmentLength(uint8_t time_signature) {
-        const int lengths[] = {4, 6, 8, 10};  // Steps per segment for each time signature
+        const int lengths[] = {8, 12, 16, 20};  // Steps per segment for each time signature
         return lengths[time_signature];
     }
 
